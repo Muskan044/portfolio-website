9dc54f111989a7162250f219d8ec51d173a02124
Muskan044 <muskankhan430430@gmail.com>
2024-09-19 23:53:48 +0500

    css and js styling of hero section done

--- script.js
+++ script.js
@@ -2,8 +2,27 @@ const hamurger = document.querySelector(".hamburger");
 
 const mobileMenu = document.querySelector(".nav-list ul");
 
+const menuItem = document.querySelectorAll(".nav-list ul li a");
+
+const header = document.querySelector(".header");
+
 hamurger.addEventListener('click', () =>{
     hamurger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
+});
 
+menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        hamurger.classList.toggle("active");
+        mobileMenu.classList.toggle("active");
+    });
+});
+
+document.addEventListener("scroll", () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = "#29323c";
+    } else {
+        header.style.backgroundColor = "transparent"
+    }
 });
